51a128e1a59beda2025b1045b3e84b5175fba481
Tai Pham <phamtantai2609@gmail.com>
2026-08-08 13:44:47 +0700

    Fix command vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-};
+const nextConfig: NextConfig = {};
 export default nextConfig;
